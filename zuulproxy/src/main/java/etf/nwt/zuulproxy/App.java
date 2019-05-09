e679a9c2936b973d363f003f2c3f6a8d82743505
Muhamed <mkapo1@etf.unsa.ba>
2019-05-09 19:13:50 +0200

    API Gateway

---
+++ zuulproxy/src/main/java/etf/nwt/zuulproxy/App.java
@@ -0,0 +1,21 @@
+package etf.nwt.zuulproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+/**
+ * Hello world!
+ *
+ */
+@EnableZuulProxy
+@SpringBootApplication
+@EnableEurekaClient
+public class App 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
